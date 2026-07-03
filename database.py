9280165f3d6f6567bd2af98bddd4e9b4f887f108
JailsonDev2005 <devjailson43@gmail.com>
2026-07-03 01:00:52 -0300

    Update database.py

--- database.py
+++ database.py
@@ -2,12 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = "sqlite:///./produtos.db"
-
+#CRIA A CONEXÃO COM O BANCO DE DADOS
 engine = create_engine(
     DATABASE_URL,
     connect_args={"check_same_thread": False}
 )
 
+#CONTRUTOR DE SESSÕES
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
